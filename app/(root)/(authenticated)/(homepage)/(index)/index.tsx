67284782d339de9d510658facd21fc4fec58444d
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2024-05-28 23:35:07 +0300

    "Refactored TabOneScreen component: rearranged state variables, removed unused functions, and updated modal messages and buttons."

--- app/(root)/(authenticated)/(homepage)/(index)/index.tsx
+++ app/(root)/(authenticated)/(homepage)/(index)/index.tsx
@@ -24,18 +24,24 @@ import MainGameContainer from "@/components/HomePageComponents/GameRenderCompone
 
 export default function TabOneScreen() {
 
-    const [falseIndexs, setFalseIndexs] = useState<any[]>([])
+    // page refresh state
+    const [refreshing, setRefreshing] = useState(false);
 
+    // arrays data
+    const [falseIndexs, setFalseIndexs] = useState<any[]>([])
     const [randomData, setRandomData] = useState<SentenceData>();
     const [shuffledResults, setShuffledResults] = useState<any[]>([]);
     const [answerData, setAnswerData] = useState<any[]>([]);
 
-    const [refreshing, setRefreshing] = useState(false);
-    const [modalVisible, setModalVisible] = useState(false);
-    const [modalMessage, setModalMessage] = useState('');
 
+    // button states
     const [helpState, setHelpState] = useState<boolean>(false);
     const [showChoosen, setShowChoosen] = useState(false); // showChoosen false onaylanmıs demek render edilebilir
+
+    // modal messages
+    const [modalMessage, setModalMessage] = useState('');
+    const [modalButtonMessage, setModalButtonMessage] = useState('');
+    const [modalVisible, setModalVisible] = useState(false);
 
     // questionrender gonderilecek
     const [choosenLevel, setChoosenLevel] = useState<{
@@ -47,8 +53,6 @@ export default function TabOneScreen() {
         single: Choosens.Random,
         selected: 'single'
     });
-
-
 
     const filterData = (data: SentenceData[]): SentenceData[] => {
         if (choosenLevel.selected === 'single' && choosenLevel.single !== Choosens.Random) {
@@ -108,12 +112,19 @@ export default function TabOneScreen() {
         if (control) {
             setModalMessage('Congratulations! You have completed the test. Please refresh to start a new test.');
             refresh();
+            setModalButtonMessage('Next')
             setModalVisible(true);
+
+
         } else {
             setModalMessage('Please try again.');
+            setModalButtonMessage('Try Again')
             setModalVisible(true);
         }
     };
+
+    // use effectss
+
     useEffect(() => {
         if (showChoosen == false)
             refresh();
@@ -126,138 +137,8 @@ export default function TabOneScreen() {
     useEffect(() => {
         controlDataResults();
     }, [answerData])
-    // function renderGetAnswerData() {
-    //     return (
-    //         <View
-    //             style={{
-    //                 minHeight: 100,
-    //                 padding: 15,
-    //                 borderColor: 'black',
-    //                 borderBottomWidth: 1,
-    //                 display: "flex",
-    //                 flexDirection: "row",
-    //                 flexWrap: "wrap",
-    //                 gap: 10,
-    //                 alignContent: "center",
-    //                 justifyContent: "center",
-    //             }}
-    //         >
-    //             {renderAnswerDataItem()}
-    //         </View>
-    //     );
-    // }
-    // function renderAnswerDataItem() {
-    //     return (
-    //         <>
 
-
-    //             {answerData.length > 0 ? (answerData.map((item: any, index: number) => {
-    //                 return (
-    //                     <Pressable
-    //                         key={index}
-    //                         style={({ pressed }) => [
-    //                             {
-    //                                 borderWidth: 1,
-    //                                 padding: 10,
-    //                                 borderRadius: 35,
-    //                                 backgroundColor: pressed ? '#e0f7fa' : '#ffffff',
-    //                                 borderColor: pressed ? '#00796b' : '#00796b',
-
-    //                                 shadowColor: '#000',
-    //                                 shadowOffset: { width: 0, height: 2 },
-    //                                 shadowOpacity: 0.2,
-    //                                 shadowRadius: 2,
-    //                                 elevation: 2, // Android için gölge efekti
-    //                             },
-    //                         ]}
-    //                         onPress={() => addToShuffledData(index)}
-    //                     >
-    //                         <Text style={{ fontSize: 20, color: "#00796b" }}>{item}</Text>
-    //                     </Pressable>
-    //                 );
-    //             })) : (<Text style={{ fontSize: 21, padding: 3, borderBottomColor: 'gray', borderBottomWidth: 1 }}>Please click on the words below. </Text>)}
-    //         </>
-    //     );
-    // }
-    // function renderSetAnswerData() {
-    //     return (
-    //         <View
-    //             style={{
-    //                 minHeight: 100,
-    //                 padding: 15,
-    //                 marginTop: 30, // 'top' yerine 'marginTop' kullanmak daha yaygın bir yöntemdir
-    //                 borderColor: "gray",
-    //                 borderBottomWidth: 1,
-    //                 display: "flex",
-    //                 flexDirection: "row",
-    //                 flexWrap: "wrap",
-    //                 gap: 10, // 'gap' property sadece webde çalışıyor. Onun yerine 'margin' kullanabiliriz
-    //                 alignContent: "center",
-    //                 justifyContent: "center",
-    //             }}
-    //         >
-    //             {renderShuffledDataItems()}
-    //         </View>
-    //     );
-    // }
-    // function renderShuffledDataItems() {
-    //     return (
-    //         <>
-    //             {shuffledResults &&
-    //                 shuffledResults.map((item: any, index: number) => {
-    //                     return (
-    //                         <Pressable
-    //                             key={index}
-    //                             style={({ pressed }) => [
-    //                                 {
-    //                                     borderWidth: 1,
-    //                                     padding: 10,
-    //                                     borderRadius: 35,
-    //                                     backgroundColor: pressed ? '#ffcc80' : '#ffffff',
-    //                                     borderColor: pressed ? '#e65100' : '#e65100',
-
-    //                                     shadowColor: '#000',
-    //                                     shadowOffset: { width: 0, height: 2 },
-    //                                     shadowOpacity: 0.2,
-    //                                     shadowRadius: 2,
-    //                                     elevation: 2, // Android için gölge efekti
-    //                                 },
-    //                             ]}
-    //                             onPress={() => addToAnswerData(index)}
-    //                         >
-    //                             <Text style={{ fontSize: 20, color: "#e65100" }}>{item}</Text>
-    //                         </Pressable>
-    //                     );
-    //                 })}
-    //         </>
-    //     );
-    // }
-    // function addToAnswerData(indexArray: number) {
-    //     setAnswerData((prevAnswerData) => [
-    //         ...prevAnswerData,
-    //         shuffledResults[indexArray],
-    //     ]);
-    //     setShuffledResults((prevShuffledResults) =>
-    //         prevShuffledResults.filter((shuffledItem, shuffledItemIndex) => {
-    //             // Check if the current shuffled item's index matches the target index
-    //             if (shuffledItemIndex === indexArray) {
-    //                 return shuffledItem !== shuffledResults[indexArray]; // Remove the item at indexArray
-    //             } else {
-    //                 return true; // Keep all other items
-    //             }
-    //         })
-    //     );
-    // }
-    // function addToShuffledData(indexArray: number) {
-    //     setShuffledResults((prevShuffledResults) => [
-    //         ...prevShuffledResults,
-    //         answerData[indexArray],
-    //     ]);
-
-    //     setAnswerData((prevAnswerData) =>
-    //         prevAnswerData.filter((prevItem) => prevItem !== answerData[indexArray])
-    //     );
-    // }
+    // use effects
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
             <ScrollView
@@ -299,7 +180,7 @@ export default function TabOneScreen() {
                                     setModalVisible(!modalVisible);
 
                                 }}>
-                                <Text style={styles.textStyle}>{modalMessage}</Text>
+                                <Text style={styles.textStyle}>{modalButtonMessage}</Text>
                             </Pressable>
                         </View>
                     </View>
